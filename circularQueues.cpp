4fdde49d116278fab8f64831866b9b50ceb142f8
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-12 10:41:14 +0700

    menampilkan elemen queue dari FRONT ke REAR jika tidak circular wrap

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -83,5 +83,16 @@ public :
         }
 
         cout << "\nElements in the queue are...\n";
+
+        //Jika FRONT_position <= REAR_position, iterasu daru front hingga rear
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            
+        }
     }    
 };
