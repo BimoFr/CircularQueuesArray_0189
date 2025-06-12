74418a650915f0b14349fc8695b08fee12aae42c
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-12 10:34:08 +0700

    menambahkan logika reset antrian saat hanya satu elemen tersisa di remove

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -54,6 +54,12 @@ public :
         }
         cout << "\nThe elemnt deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
         
     } 
 };
