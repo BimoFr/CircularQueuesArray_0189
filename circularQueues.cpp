24be8f3624a4fc7728664c5a47607ff6e9daf5ea
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-12 10:26:21 +0700

    implementasi penuh fungsi insert untuk circular queue

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -38,8 +38,10 @@ public :
         // Jika REAR berada di posisi terakhir array, kembali ke awal array
         if (REAR == max - 1)
             REAR = 0;
+        else
+            REAR = REAR + 1;
     }
+    queue_array[REAR] = num;
     }
-
     
 };
